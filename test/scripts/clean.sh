8d122eef385e2df9af6ace7a7411b3ec201fee4c
icfr <contact@icfr.fr>
2016-09-22 20:22:49 +0200

    [test] Update clean script

--- test/scripts/clean.sh
+++ test/scripts/clean.sh
@@ -6,3 +6,4 @@ find "${BASEDIR}/data/base/Commands/" -type f ! -regex ".*/\(readme\)" -exec rm 
 find "${BASEDIR}/data/base/resources/langs/" -type f ! -regex ".*/\(readme\)" -exec rm {} \;
 rm -Rf "${BASEDIR}/data/base/ModelsNew"
 rm -f "${BASEDIR}/data/base/Models/User.js"
+git checkout HEAD -- data/base/Config/laravel.js
