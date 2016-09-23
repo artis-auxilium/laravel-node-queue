4d05c5a992afb718b11c224ca7cd818f2fc44406
icfr <contact@icfr.fr>
2016-09-23 02:58:42 +0200

    [test] Check if nodeConfig allready created

--- test/Commands/testInit.js
+++ test/Commands/testInit.js
@@ -1,6 +1,7 @@
 'use strict';
 /* global app,appdir*/
 /* eslint global-require: 0 */
+/* eslint max-len: 0 */
 
 var rewire = require('rewire');
 var path = require('path');
@@ -9,16 +10,44 @@ var shell = require('../../lib/shell');
 var init = require('../../Commands/init');
 var laravelConfig = require('../../Commands/laravelConfig');
 var intercept = require('intercept-stdout');
+var each = require('lodash/each');
+
+var stdout;
+var unhookIntercept;
+
 module.exports = {
-  'test init': function test1(test) {
-    var stdout = [];
+  setUp: function setUp(cb) {
+    stdout = [];
     process.argv = ['node', appdir + '/artisan'];
 
     var laraveldir = path.resolve(__dirname, '../data/laravel_fa');
-    bddStdin('ini\t', '\n', '\t', laraveldir + '\t', '\n', '\n', ' \n');
+    bddStdin([
+      'ini\t',
+      '\n',
+      '\t',
+      'lib',
+      '\t',
+      '\t',
+      bddStdin.keys.delete,
+      bddStdin.keys.delete,
+      bddStdin.keys.delete,
+      bddStdin.keys.delete,
+      bddStdin.keys.delete,
+      bddStdin.keys.delete,
+      bddStdin.keys.delete,
+      bddStdin.keys.delete,
+      bddStdin.keys.delete,
+      bddStdin.keys.delete,
+      laraveldir,
+      '\t',
+      '\t',
+      '\n',
+      '\n',
+      ' \n'
+    ]);
 
-    var unhookIntercept = intercept(function onIntercept(txt) {
-      stdout.push(txt);
+    unhookIntercept = intercept(function onIntercept(txt) {
+      stdout.push(txt.replace(/\u001b\[.*?m/g, ''));
       // return '';
     });
     global.app = new shell({
@@ -43,8 +72,58 @@ module.exports = {
     })
     app.cmd(init.pattern, init.help, init.function);
     app.cmd(laravelConfig.pattern, laravelConfig.help, laravelConfig.function);
+    cb();
+  },
+  'test init': function testInit(test) {
+
     process.stdin.destroy = function stdinDestroy() {
       unhookIntercept();
+      var toTest = [
+        'path to laravel project: ',
+        'install command on laravel v5.2.43',
+        'path to commands folder: [app/Console/Commands/] ',
+        'command copied to /var/node/test-laraqueue/node_modules_fake/laravel-queue/test/data/laravel_fake/app/Console/Commands/NodeConfig.php',
+        'add "\\App\\Console\\Commands\\NodeConfig::class" to /var/node/test-laraqueue/node_modules_fake/laravel-queue/test/data/laravel_fake/app/Console/kernel.php',
+        'when done type enter ',
+        'services created',
+        'queue created',
+        'broadcasting created',
+        'auth created',
+        'filesystems created',
+        'database created',
+        'mail created',
+        'app created'
+      ]
+      each(toTest, function eachToTest(value) {
+        test.ok(stdout.indexOf(value) > -1);
+      });
+      test.done();
+    }
+
+  },
+  'test init allready': function testInitAllready(test) {
+
+    process.stdin.destroy = function stdinDestroy() {
+      unhookIntercept();
+      var toTest = [
+        'path to laravel project: ',
+        'install command on laravel v5.2.43',
+        'path to commands folder: [app/Console/Commands/] ',
+        'NodeConfig.php allready in your laravel project',
+        'maybe you need to add "\\App\\Console\\Commands\\NodeConfig::class" to /var/node/test-laraqueue/node_modules_fake/laravel-queue/test/data/laravel_fake/app/Console/kernel.php',
+        'when done type enter ',
+        'services created',
+        'queue created',
+        'broadcasting created',
+        'auth created',
+        'filesystems created',
+        'database created',
+        'mail created',
+        'app created'
+      ]
+      each(toTest, function eachToTest(value) {
+        test.ok(stdout.indexOf(value) > -1);
+      });
       test.done();
     }
 
