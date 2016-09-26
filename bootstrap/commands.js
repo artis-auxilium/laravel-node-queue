6186cac36049af219ddf9f9963555aaeb90cd280
icfr <contact@icfr.fr>
2016-09-26 18:51:25 +0200

    [feat] add init and start to shell

--- bootstrap/commands.js
+++ bootstrap/commands.js
@@ -1,43 +1,51 @@
 'use strict';
-/* global app,config,appdir*/
+/* global app,appdir*/
 
 var each = require('lodash/each');
 var includeAll = require('include-all');
 var path = require('path');
+var utils = require('../lib/utils');
+var Shell = require('../lib/shell');
+
+global.appdir = utils.workspace();
+
+global.app = new Shell();
 require('.');
-var shell = require('../lib/shell');
-global.app = new shell({
+app.init({
   chdir: appdir + '/'
 });
-app.config = config;
+
 app.configure(function configureApp() {
-  app.use(shell.history({
+  app.use(Shell.history({
     shell: app
   }));
-  app.use(shell.completer({
+  app.use(Shell.completer({
     shell: app
   }));
-  app.use(shell.router({
+  app.use(Shell.router({
     shell: app
   }));
-  app.use(shell.help({
+  app.use(Shell.help({
     shell: app,
     introduction: true
   }));
 });
 var eachCommands = function eachCommands(command) {
   app.cmd(command.pattern, command.help, command.function);
-}
+};
 
 var commands = includeAll({
   dirname: path.normalize(path.join(__dirname, '/../Commands/')),
   filter: /(.+)\.js$/
 });
+
 each(commands, eachCommands);
 
 var userCommands = includeAll({
   dirname: path.normalize(path.join(appdir, '/Commands/')),
   filter: /(.+)\.js$/
 });
+
 each(userCommands, eachCommands);
+app.start();
 
